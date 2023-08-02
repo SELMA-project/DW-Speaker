d21780a4ae7cad2e18d9101733382999b912a32e
Andy Giefer <andreas.giefer@dw.com>
2023-08-02 14:11:36 +0200

    Initial Commit

---
+++ DW Speaker/DW_SpeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DW_SpeakerApp.swift
+//  DW Speaker
+//
+//  Created by Andy Giefer on 02.08.23.
+//
+
+import SwiftUI
+
+@main
+struct DW_SpeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
