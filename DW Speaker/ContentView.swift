b3639aa831a1b6f5496e7d0d20e309365497df04
Andy Giefer <andreas.giefer@dw.com>
2023-08-02 14:49:09 +0200

    Initial commit

--- DW Speaker/ContentView.swift
+++ DW Speaker/ContentView.swift
@@ -8,19 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textToSpeak: String = "Hello, my name is Andy."
+    
+    @State var selectedLanguage: Language = .English
+    enum Language: String, CaseIterable {
+        case German, English, Hindi, Urdu
+    }
+
+    @State var selectedVoiceProvider: VoiceProvider = .Apple
+    enum VoiceProvider: String, CaseIterable {
+        case Apple, Azure, Google, ElevenLabs
+    }
+
+    @State var selectedVoice: Voice = .Andy
+    enum Voice: String, CaseIterable {
+        case Andy, Johann, Alice
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            TextEditor(text: $textToSpeak)
+                .font(.title2)
+                .padding(40)
+                .background(ignoresSafeAreaEdges: .all)
+            
+            HStack {
+                Picker("Language:", selection: $selectedLanguage) {
+                    ForEach(Language.allCases, id: \.self) { language in
+                        Text(language.rawValue).tag(language)
+                    }
+                }
+                
+                Picker("Provider:", selection: $selectedVoiceProvider) {
+                    ForEach(VoiceProvider.allCases, id: \.self) { provider in
+                        Text(provider.rawValue).tag(provider)
+                    }
+                }
+                
+                Picker("Voice:", selection: $selectedVoice) {
+                    ForEach(Voice.allCases, id: \.self) { voice in
+                        Text(voice.rawValue).tag(voice)
+                    }
+                }
+                
+                Button("Speak") {
+                    print("Speaking")
+                }
+                .buttonStyle(BlueButtonStyle())
+            }
+            .padding()
+
         }
-        .padding()
+        
+    }
+}
+
+struct BlueButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+            .foregroundColor(configuration.isPressed ? Color.blue : Color.white)
+            .background(configuration.isPressed ? Color.white : Color.blue)
+            .cornerRadius(6)
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(minWidth: 800, minHeight: 600)
     }
 }
--- DW Speaker/DW_SpeakerApp.swift
+++ DW Speaker/DW_SpeakerApp.swift
@@ -12,6 +12,7 @@ struct DW_SpeakerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 800, minHeight: 600)
         }
     }
 }
