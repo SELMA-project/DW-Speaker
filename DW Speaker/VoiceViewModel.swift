39fe3532aebc1190836348f2bcdd3a79907339bd
Andy Giefer <andreas.giefer@dw.com>
2023-08-17 13:53:24 +0200

    Storying IDs in UserDefaults works

--- DW Speaker/VoiceViewModel.swift
+++ DW Speaker/VoiceViewModel.swift
@@ -22,23 +22,33 @@ class VoiceViewModel: ObservableObject {
     @Published var selectedLocaleId: String = "" {
         didSet {
             
-            // after the Locale changes, adjust the list of selectable provider and and choose the first on the list
-            let selectedLocale = Locale(identifier: selectedLocaleId)
-            Task {
+            if oldValue != selectedLocaleId {
                 
-                let providers = await voiceController.providers(forLocale: selectedLocale)
-                DispatchQueue.main.async {
-                    
-                    // set list of providers
-                    self.selectableProviders = providers
-                    
-                    // select first provider on the list
-                    if let firstProvider = providers.first {
-                        self.selectedProviderId = firstProvider.id
-                    }
-                }
-        
+                print("Updating selectedLocaleId to: \(selectedLocaleId)")
+                
+                // store in user defaults
+                UserDefaults.standard.set(selectedLocaleId, forKey: UserDefaultKeys.selectedLocaleIdName)
+                
+                ensureConsistency()
             }
+//
+//            // after the Locale changes, adjust the list of selectable provider and and choose the first on the list
+//            let selectedLocale = Locale(identifier: selectedLocaleId)
+//            Task {
+//
+//                let providers = await voiceController.providers(forLocale: selectedLocale)
+//                DispatchQueue.main.async {
+//
+//                    // set list of providers
+//                    self.selectableProviders = providers
+//
+//                    // select first provider on the list
+//                    if let firstProvider = providers.first {
+//                        self.selectedProviderId = firstProvider.id
+//                    }
+//                }
+//
+//            }
         }
     }
     
@@ -46,27 +56,109 @@ class VoiceViewModel: ObservableObject {
     @Published var selectedProviderId: String = "" {
         didSet {
             
-            // after the provider changes, adjust the list of selectable voice and choose the prefered one
-            if let selectedProvider {
-                Task {
-                    let selectedLocale = Locale(identifier: selectedLocaleId)
-                    let voices = await selectedProvider.availableVoicesForLocale(locale: selectedLocale)
-                    let preferedVoice = await selectedProvider.preferedVoiceForLocale(locale: selectedLocale)
-                    
-                    DispatchQueue.main.async {
-                        self.selectableVoices = voices
-                        
-                        if let preferedVoice {
-                            self.selectedVoiceId = preferedVoice.id
-                        }
-                    }
-                }
+            if oldValue != selectedProviderId {
+                
+                print("Updating selectedProviderId to: \(selectedProviderId)")
+                // store in user defaults
+                UserDefaults.standard.set(selectedProviderId, forKey: UserDefaultKeys.selectedProviderIdName)
+                
+                ensureConsistency()
+                
             }
+            
+//            // after the provider changes, adjust the list of selectable voice and choose the prefered one
+//            if let selectedProvider {
+//                Task {
+//                    let selectedLocale = Locale(identifier: selectedLocaleId)
+//                    let voices = await selectedProvider.availableVoicesForLocale(locale: selectedLocale)
+//                    let preferedVoice = await selectedProvider.preferedVoiceForLocale(locale: selectedLocale)
+//
+//                    DispatchQueue.main.async {
+//                        self.selectableVoices = voices
+//
+//                        if let preferedVoice {
+//                            self.selectedVoiceId = preferedVoice.id
+//                        }
+//                    }
+//                }
+//            }
         }
     }
     
     /// The identifier of the selected voice.
-    @Published var selectedVoiceId: String = ""
+    @Published var selectedVoiceId: String = "" {
+        didSet {
+            //if oldValue != selectedVoiceId {
+                
+                print("Updating selectedVoiceId to: \(selectedVoiceId)")
+                
+                // store in user defaults
+                UserDefaults.standard.set(selectedVoiceId, forKey: UserDefaultKeys.selectedVoiceIdName)
+            //}
+        }
+    }
+    
+    /// Looks at current Ids and updates them if need to keep consistency
+    private func ensureConsistency()   {
+
+        Task {
+            
+            // read current ids
+            let proposedLocaleId = selectedLocaleId
+            let proposedProviderId = selectedProviderId
+            let proposedVoiceId = selectedVoiceId
+            
+            // the proposed Locale becomes the recommended Locale
+            let recommendedLocaleId = proposedLocaleId
+            
+            // convert locale id to locale and get all availabe providers
+            let selectedLocale = Locale(identifier: proposedLocaleId)
+            let providers = await voiceController.providers(forLocale: selectedLocale)
+            
+            // find provider for proposedProviderId. Fallback: use first on the list
+            let recommendedProvider = providers.first(where: { $0.id == proposedProviderId}) ?? providers.first
+            let recommendedProviderId = recommendedProvider?.id
+            
+            // find voice for recommended provider
+            var recommendedVoiceId: String? = nil
+            var voices = [Voice]()
+            if let recommendedProvider {
+                voices = await recommendedProvider.availableVoicesForLocale(locale: selectedLocale) // get all voices
+                let fallbackVoiceVoice = await recommendedProvider.preferedVoiceForLocale(locale: selectedLocale) // used if proposedVoiceId does not match
+                let recommendedVoice = voices.first(where: { $0.id == proposedVoiceId}) ?? fallbackVoiceVoice
+                recommendedVoiceId = recommendedVoice?.id
+            }
+            
+            // publish properties
+            DispatchQueue.main.async {
+                
+                // publish locale
+                self.selectedLocaleId = recommendedLocaleId
+                
+                // publish provider if it has changed
+                self.selectableProviders = providers
+                if recommendedProviderId != proposedProviderId {
+                    if let recommendedProviderId {
+                        self.selectedProviderId = recommendedProviderId
+                    }
+                }
+                
+                // publish voice if it has changed
+                self.selectableVoices = voices
+                if recommendedVoiceId != proposedVoiceId {
+                    if let recommendedVoiceId {
+                        self.selectedVoiceId = recommendedVoiceId
+                    }
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    
+
     
     /// Stores the individual settings for all voices.
     @Published var voiceSettings = VoiceSettings()
@@ -81,7 +173,66 @@ class VoiceViewModel: ObservableObject {
     }
     
     @Published var playerStatus: PlayerStatus = .idle
+    
+    struct UserDefaultKeys {
+        static let selectedLocaleIdName = "voiceViewModelSelectedLocaleId"
+        static let selectedProviderIdName = "voiceViewModelSelectedProviderId"
+        static let selectedVoiceIdName = "voiceViewModelSelectedVoiceId"
+    }
+    
+    private func restoreDefaults() {
         
+        Task {
+        
+            // which provider and locale  should be shosen when the app starts the first time?
+            let defaultLocaleId = "en-US"
+            let defaultProvider = AppleVoiceProvider()
+            
+            // derive ids
+            let defaultProviderId = defaultProvider.id
+            let defaultVoiceId = await defaultProvider.preferedVoiceForLocale(locale: Locale(identifier: defaultLocaleId))!.id
+            
+            // register defaults
+            UserDefaults.standard.register(defaults: [
+                UserDefaultKeys.selectedLocaleIdName : defaultLocaleId,
+                UserDefaultKeys.selectedProviderIdName: defaultProviderId,
+                UserDefaultKeys.selectedVoiceIdName: defaultVoiceId,
+            ])
+            
+            // When the app was already launched before, the USerDefaults are set, so the registered defaults do not count
+            
+            // restore selected locale from User Defaults
+            let localekey = UserDefaultKeys.selectedLocaleIdName
+            let selectedLocaleId = UserDefaults.standard.string(forKey: localekey)!
+            let selectedLocale = Locale(identifier: selectedLocaleId)
+            
+            // restore selected provider from User Defaults
+            let providerKey = UserDefaultKeys.selectedProviderIdName
+            let selectedProviderId = UserDefaults.standard.string(forKey: providerKey)!
+            let locales = await voiceController.availableLocales() // all available locales
+            let providers = await voiceController.providers(forLocale: selectedLocale) // all available providers
+            let selectedProvider = providers.first { $0.id == selectedProviderId} ?? AppleVoiceProvider() // derive selectedProvider based on its ID
+            
+            // restore selected voiceId from User Defaults
+            let voiceKey = UserDefaultKeys.selectedVoiceIdName
+            let selectedVoiceId = UserDefaults.standard.string(forKey: voiceKey)!
+            
+            // publish
+            DispatchQueue.main.async {
+                self.selectableLocales = locales
+                self.selectableProviders = providers
+                
+                self.selectedLocaleId = selectedLocale.identifier
+                self.selectedProviderId = selectedProvider.id
+                self.selectedVoiceId = selectedVoiceId
+                
+                self.ensureConsistency()
+            }
+            
+        }
+        
+    }
+    
     init() {
         
         /// Access to voice functionalitites
@@ -90,24 +241,25 @@ class VoiceViewModel: ObservableObject {
         /// Access to voice functionalitites
         self.audioPlayerController = AudioPlayerController()
         
-        // default locale and provider
-        let selectedLocale = Locale(identifier: "en-US")
-        let selectedProvider = AppleVoiceProvider()
+        restoreDefaults()
         
-        // set available locales and providers
-        Task {
-            let locales = await voiceController.availableLocales()
-            let providers = await voiceController.providers(forLocale: selectedLocale)
-            
-            DispatchQueue.main.async {
-                self.selectableLocales = locales
-                self.selectableProviders = providers
-                
-                self.selectedLocaleId = selectedLocale.identifier
-                self.selectedProviderId = selectedProvider.id
-                
-            }
-        }
+//        // restore provider from UserDefaults
+//        let selectedProvider = AppleVoiceProvider()
+//
+//        // set available locales and providers
+//        Task {
+//            let locales = await voiceController.availableLocales()
+//            let providers = await voiceController.providers(forLocale: selectedLocale)
+//
+//            DispatchQueue.main.async {
+//                self.selectableLocales = locales
+//                self.selectableProviders = providers
+//
+//                self.selectedLocaleId = selectedLocale.identifier
+//                self.selectedProviderId = selectedProvider.id
+//
+//            }
+//        }
     }
     
     func speak(text: String) {
