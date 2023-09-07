2e949c9a86c17c07836ba48a7ec8596f5d25d57a
Andy Giefer <andreas.giefer@dw.com>
2023-09-07 09:59:40 +0200

    Added styleExaggeration and speakerBoost to ElevenLAbs API Call

--- DW Speaker/ElevenLabsVoiceSettingsView.swift
+++ DW Speaker/ElevenLabsVoiceSettingsView.swift
@@ -32,7 +32,7 @@ struct ElevenLabsVoiceSettingsView: View {
     var styleSlider: some View {
         HStack {
             Slider(value: $voiceViewModel.voiceSettings.elevenLabs.styleExaggeration, in: 0...1) {
-                Text("Style Exaggeration:")
+                Text("Exaggeration:")
             }
             Text("\(voiceViewModel.voiceSettings.elevenLabs.styleExaggeration.formatted(.number.precision(.fractionLength(1))))")
         }
