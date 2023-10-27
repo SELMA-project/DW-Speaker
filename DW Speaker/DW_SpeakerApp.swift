c427f592cd63d35d92127344b1378034a2a9ed44
Andy Giefer <andreas.giefer@dw.com>
2023-10-27 17:46:30 +0200

    Added voice discovery

--- DW Speaker/DW_SpeakerApp.swift
+++ DW Speaker/DW_SpeakerApp.swift
@@ -13,16 +13,12 @@ import DWSpeakerKit
 @main
 struct DW_SpeakerApp: App {
     
-    let priberamVoiceManager = PriberamVoiceManager(executeTest: true)
+    let priberamGoogleVoiceManager = PriberamVoiceManager(voiceSubProvider: .google, executeTest: true)
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .frame(minWidth: 800, minHeight: 600)
-//                .task {
-//                    let priberamVoiceManager = PriberamVoiceManager()
-//                    await priberamVoiceManager?.test()
-//                }
         }
         
 #if os(macOS)
